30f24bbd515e92d8ae68684bd862159aa98d7815
Andy Gauge <andygauge@gmail.com>
2019-01-23 10:03:12 -0800

    authorize host

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module BbsmApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.hosts << "api.beboldstreetministries.org"
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
