adc312cd95c2415a0c11525b14862dc1fa314bca
Andy Gauge <andygauge@gmail.com>
2019-01-23 13:37:14 -0800

    JSON parsing

--- app/models/striped/webhook.rb
+++ app/models/striped/webhook.rb
@@ -1,16 +1,11 @@
 class Striped::Webhook < ApplicationRecord
+  serialize :event, JSON
   belongs_to :customer, class_name: "Striped::Customer", foreign_key: 'striped_customer_id', optional:true
 
   before_create :find_customer
 
-  def parsed_event
-    return @event if @event
-    @event= JSON.parse(event) if event.is_a? String
-    @event = event
-  end
-
   def find_customer
-    return false unless cust = parsed_event.dig("object", "customer")
+    return false unless cust = event.dig("object", "customer")
     customer = Striped::Customer.find_by_stripe_id cust
   end
 end
