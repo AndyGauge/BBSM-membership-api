2463ae36da7c0b51fb30c95ac8115c9eaa0a42ea
Andy Gauge <andygauge@gmail.com>
2019-01-23 12:44:36 -0800

    log the data

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,6 @@
 class API::V1::EventsController < ApplicationController
   def create
-    Striped::Webhook.create(event: Stripe::Event.retrieve(params[:id]))
+    Striped::Webhook.create(event: params.[:data])
     render nothing: true, status: 200
   rescue Stripe::APIConnectionError, Stripe::StripeError
       render nothing: true, status: 400
